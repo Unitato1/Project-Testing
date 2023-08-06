7908cb4279813a58c117db2dc68854285c78c814
Unitato1 <robertsvihla@gmail.com.github.com>
2023-08-06 12:28:05 +0200

    Finished Tests and Function capitalize

---
+++ capitalize.js
@@ -0,0 +1,3 @@
+export default function capitalize(sentence) {
+  return sentence.charAt(0).toUpperCase() + sentence.slice(1);
+}
---
+++ capitalize.test.js
@@ -0,0 +1,22 @@
+import capitalize from "./capitalize.js";
+
+test("One word sentence", () => {
+  expect(capitalize("word")).toMatch("Word");
+});
+test("No word sentence", () => {
+  expect(capitalize("")).toMatch("");
+});
+
+test("Sentence", () => {
+  expect(capitalize("words are art.")).toMatch("Words are art.");
+});
+
+test("Really long sentence", () => {
+  expect(
+    capitalize(
+      "words are art which many people use unwisely, this leads to empty words."
+    )
+  ).toMatch(
+    "Words are art which many people use unwisely, this leads to empty words."
+  );
+});
